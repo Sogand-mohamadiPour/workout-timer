3dded60456b45080ba9f3c8943a3817c1f88aa00
Sogand <sogandmohamadipour7@gmail.com>
2025-12-25 20:54:25 +0330

    audio fixed

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useEffect, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -9,29 +9,33 @@ function Calculator({ workouts, allowSound }) {
 
   const [duration, setDuration] = useState(0);
 
+
+
+
   useEffect(function () {
-    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak)
+    setDuration((number * sets * speed) / 60 + (sets - 1) * durationBreak);
   }, [number, sets, speed, durationBreak]);
 
+  useEffect(function () {
+    const playSound = function () {
+      if (!allowSound) return;
+      const sound = new Audio(clickSound);
+      sound.play();
+    };
+    playSound();
+  }, [duration, allowSound]);
 
 
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
 
   function handleInc() {
     setDuration((duration) => Math.floor(duration + 1));
-    playSound();
   }
 
   function handleDec() {
     setDuration((duration) => duration > 1 ? Math.ceil(duration - 1) : 0);
-    playSound();
   }
 
   return (
